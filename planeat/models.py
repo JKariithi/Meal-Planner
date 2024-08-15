541a517a56c79e00c9d0957bcd071f7d2b452d5f
JKariithi <kariithijanice@gmail.com>
2024-08-15 08:17:27 +0300

    Defining the Days of the week Model

--- planeat/models.py
+++ planeat/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Days(models.Model):
+    """The Day of the week"""
+    day = models.CharField(max_length=50)
+
+
+    def __str__(self):
+        """A representation of the model"""
+        return self.day
+
